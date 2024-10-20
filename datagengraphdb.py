bb02647ccf83f8a2a231322132674105e2353d99
krzychu-bk <115548542+krzychu-bk@users.noreply.github.com>
2024-10-20 22:35:19 +0200

    added price and transfer time as random ints

--- datagengraphdb.py
+++ datagengraphdb.py
@@ -13,7 +13,8 @@ def generate_fake_airports(num_airports=10):
             "code": fake.unique.lexify(text='???').upper(),  # Generate a random 3-letter code
             "name": f"{fake.city()} International Airport",
             "city": fake.city(),
-            "country": fake.country()
+            "country": fake.country(),
+            "transfer_time": random.randint(20, 120)
         }
         airports.append(airport)
     return airports
@@ -33,33 +34,35 @@ def generate_random_flights(airports, num_flights=10):
         departure_time = datetime.datetime.now() + datetime.timedelta(minutes=random.randint(30, 300))
         arrival_time = departure_time + datetime.timedelta(minutes=random.randint(60, 600))
         duration = (arrival_time - departure_time).seconds // 60
-        
+        price = random.randint(200,2000)
         flight = {
             "flight_number": flight_number,
             "departure_airport": departure_airport["code"],
             "arrival_airport": arrival_airport["code"],
             "departure_time": departure_time.strftime("%Y-%m-%dT%H:%M:%S"),
             "arrival_time": arrival_time.strftime("%Y-%m-%dT%H:%M:%S"),
-            "duration": duration
+            "duration": duration,
+            "price": price,
         }
         flights.append(flight)
     return flights
 
 # Generate fake airports and flights
-airports = generate_fake_airports(10)  # Generate 10 fake airports
-flights = generate_random_flights(airports, 15)  # Generate 15 random flights
+airports = generate_fake_airports(50)  # Generate n fake airports
+flights = generate_random_flights(airports, 200)  # Generate m random flights
 
 # Generate Cypher queries to insert the data into Neo4j
 def generate_cypher_queries(airports, flights):
     # Create queries for airports
     airport_queries = []
+    i = 0
     for airport in airports:
         airport_query = f"""
-        MERGE (a:Airport {{code: '{airport['code']}'}})
-        ON CREATE SET a.name = '{airport['name']}', a.city = '{airport['city']}', a.country = '{airport['country']}'
+        MERGE (a{i}:Airport {{code: '{airport['code']}'}})
+        ON CREATE SET a.name = '{airport['name']}', a.city = '{airport['city']}', a.country = '{airport['country']}', a.transfer_time = {airport['transfer_time']}'
         """
         airport_queries.append(airport_query.strip())
-    
+        i = i + 1  
     # Create queries for flights
     flight_queries = []
     for flight in flights:
@@ -70,7 +73,8 @@ def generate_cypher_queries(airports, flights):
             flight_number: '{flight['flight_number']}',
             departure_time: datetime('{flight['departure_time']}'),
             arrival_time: datetime('{flight['arrival_time']}'),
-            duration: {flight['duration']}
+            duration: {flight['duration']},
+            price: {flight['price']}
         }}]->(destination)
         """
         flight_queries.append(flight_query.strip())
