94bac15f20b33fd8a455c0fbf7c4648535e20f15
krzychu-bk <115548542+krzychu-bk@users.noreply.github.com>
2024-10-20 20:55:48 +0200

    added dynamic id in queries

--- datagengraphdb.py
+++ datagengraphdb.py
@@ -16,16 +16,23 @@ def rm_att_quotes(json_like_str):
 
 def gencyphernodes(data):
     creates = []
+    i = 0
     for node in data:
-        query = "CREATE(n:Airport" + node + ")"
+        query = "CREATE(a"+str(i)+":Airport" + node + ")"
         creates.append(query)
+        i = i + 1
     return creates
 
 airport_data = []
+IATAcollection = []
 for _ in range(50):
-    airport_data.append(rm_att_quotes(fake.airport_object()))
-
+    airport = fake.airport_object()
+    iata = airport
+    airport_data.append(rm_att_quotes(airport))
+    IATAcollection.append(iata)
 
 with open("airport_queries.txt", "w") as f:
         for query in gencyphernodes(airport_data):
             f.write(query + "\n\n")
+
+print(IATAcollection)
